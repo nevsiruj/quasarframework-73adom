19c3864ec2e00cf8e73a105f75446c3d96bd1bbf
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-01-25 11:36:45 -0300

    1 file modified

--- src/components/tpv/ventas/views/TpvVentas.vue
+++ src/components/tpv/ventas/views/TpvVentas.vue
@@ -1,5 +1,30 @@
 <template>
   <h3>Hola</h3>
+
+  <q-form @submit="createSale" ref="form" v-if="showForm">
+    <q-input
+      v-model="sale.name"
+      label="Nombre del producto"
+      :rules="nameRules"
+      type="text"
+      required
+    />
+    <q-input
+      v-model="sale.quantity"
+      label="Cantidad"
+      :rules="quantityRules"
+      type="number"
+      required
+    />
+    <q-input
+      v-model="sale.price"
+      label="Precio"
+      :rules="priceRules"
+      type="number"
+      required
+    />
+    <q-btn color="primary" type="submit" label="Crear venta" />
+  </q-form>
 </template>
 
 <script>
@@ -26,6 +51,44 @@ export default defineComponent({
     icon: {
       type: String,
       default: '',
+    },
+  },
+  data() {
+    return {
+      showForm: true,
+      sale: {
+        name: '',
+        quantity: 0,
+        price: 0,
+      },
+      nameRules: [
+        (v) => !!v || 'Nombre del producto es requerido',
+        (v) =>
+          (v && v.length <= 20) ||
+          'Nombre del producto debe tener 20 caracteres o menos',
+      ],
+      quantityRules: [
+        (v) => !!v || 'Cantidad es requerida',
+        (v) => (v && v > 0) || 'La cantidad debe ser mayor a 0',
+      ],
+      priceRules: [
+        (v) => !!v || 'Precio es requerido',
+        (v) => (v && v > 0) || 'El precio debe ser mayor a 0',
+      ],
+    };
+  },
+  methods: {
+    createSale() {
+      this.$refs.form.validate().then((valid) => {
+        if (valid) {
+          // Enviar sale a la API o base de datos
+          this.sale = {
+            name: '',
+            quantity: 0,
+            price: 0,
+          };
+        }
+      });
     },
   },
 });
